d73d0ba164f9cc0b6e8f5fbbad9082032892dead
gafaradetunji <gafaradetunji471@gmail.com>
2024-01-02 09:55:45 +0100

    pushing beans

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1>Project Harmony</h1>
+      <h1 className="text-[5rem] text-[#df0000]">Project Harmony</h1>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </main>
-  )
+  );
 }
